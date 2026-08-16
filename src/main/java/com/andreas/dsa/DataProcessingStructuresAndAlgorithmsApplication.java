5ef9b0838908d474de20e02fa4576f8867329245
Andreas Weibert <84761487+andreas0x07@users.noreply.github.com>
2026-08-16 19:32:11 +0200

    feat: create initial spring boot project

---
+++ src/main/java/com/andreas/dsa/DataProcessingStructuresAndAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.andreas.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessingStructuresAndAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessingStructuresAndAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreas/dsa/DataProcessingStructuresAndAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreas.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessingStructuresAndAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
